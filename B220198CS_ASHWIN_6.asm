799174471f56e14b2b0ca580e8b7f2bf6b6b8acd
ashwinnair10 <ashwinnair1894@gmail.com>
2024-03-06 16:11:23 +0530

    mod

--- B220198CS_ASHWIN_6.asm
+++ B220198CS_ASHWIN_6.asm
@@ -12,18 +12,31 @@ main:
 	move $a0,$v0
 	move $a1,$a0
 	move $a2,$a0
+	move $a3,$a0
 	addi $t0,$zero,0
+	addi $t6,$zero,0
 	addi $t1,$zero,10
+count:
+	beq $a3,$zero,loop
+	div $a3,$t1
+	mflo $a3
+	addi $t6,$t6,1
+	j count
 loop:
 	seq $t2,$a2,$zero
 	bne $t2,$zero,test
 	div $a2,$t1
 	mfhi $t3
 	mflo $a2
-	move $t4,$t3
-	mul $t3,$t3,$t3
-	mul $t3,$t3,$t4
-	add $t0,$t0,$t3
+	li $t4,1
+	li $t5,0
+	power:
+		beq $t5,$t6,next
+		mul $t4,$t4,$t3
+		addi $t5,$t5,1
+		j power		
+	next:
+	add $t0,$t0,$t4
 	j loop
 test:
 	beq $a1,$t0,yes
